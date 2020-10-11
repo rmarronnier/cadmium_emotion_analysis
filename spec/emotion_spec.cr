275f1ceaf2653bf7dfdce387919a99e5e6aeebf6
Rémy Marronnier <rmarronnier@gmail.com>
2020-10-12 00:19:59 +0200

    1st commit

---
+++ spec/emotion_spec.cr
@@ -0,0 +1,27 @@
+require "./spec_helper"
+
+describe Cadmium::EmotionAnalysis do
+  subject = Cadmium::EmotionAnalysis::Intensity.new
+
+  it "should have language keys" do
+    subject.data.language_word_emotion_score_hash.keys.should eq(["ps", "en", "es", "kn", "km", "sn", "sl", "zh", "sv", "so", "hi", "ha", "be", "fa", "yi", "id", "sm", "am", "my", "zu", "tl", "sq", "it", "ku", "lb", "th", "lv", "uz", "ta", "gd", "ig", "ne", "ar", "ceb", "lt", "is", "jw", "ml", "lo", "te", "ur", "yo", "bg", "fi", "la", "mg", "et", "si", "pl", "hmn", "sw", "de", "ca", "xh", "eu", "eo", "hy", "bn", "tg", "sk", "pa", "no", "ga", "fy", "ro", "az", "mi", "gl", "nl", "ms", "el", "su", "sr", "ru", "ny", "ja", "fr", "mk", "kk", "gu", "vi", "bs", "uk", "hr", "ko", "hu", "co", "ht", "tr", "pt", "mn", "cy", "af", "ky", "iw", "da", "ka", "mt", "mr", "sd", "cs", "st", "haw"])
+  end
+  it "should detect angryness" do
+    subject.analyse(ANGRY_TEXT).should eq(true)
+  end
+  it "should detect whatever" do
+    subject.analyse(BPD_TEXT).should eq(true)
+  end
+  it "should detect sadness" do
+    subject.analyse(BPD_TEXTT).should eq(true)
+  end
+  it "should detect sadness2" do
+    subject.analyse(SAD_TEXT).should eq(true)
+  end
+  it "should detect angry2" do
+    subject.analyse(SAD_TEXT).should eq(true)
+  end
+  it "should detect angry3" do
+    subject.analyse(ANGRR).should eq(true)
+  end
+end
---
+++ src/cadmium/emotion_analysis/emotion_analysis.cr
@@ -0,0 +1,74 @@
+require "json"
+
+module Cadmium
+  module EmotionAnalysis
+    enum Emotion
+      Anger
+      Anticipation
+      Disgust
+      Fear
+      Joy
+      Sadness
+      Surprise
+      Trust
+    end
+
+    # alias LanguageModel = Hash(String, Tuple(Emotion, Float16))
+    # # alias ModelsHash = Hash(String, LanguageModel)
+
+    #     struct ModelsHash
+    #       property Language_word_emotion_intensity : Hash(String, LanguageModel)
+
+    #       def initialize(data : Hash(String, Tuple(Emotion, Float16)) = Hash(String, Tuple(Emotion, Float16)).new)
+    #         @Language_word_emotion_intensity = data
+    #       end
+    #     end
+
+    struct AllLanguagesModel
+      include JSON::Serializable
+      property language_word_emotion_score_hash : Hash(String, Hash(String, Tuple(Emotion, Float32)))
+
+      def initialize(hash)
+        @language_word_emotion_score_hash = hash
+      end
+    end
+
+    class Intensity
+      include Tokenizer
+      # include StopWords TODO : rework the whole language symbol / string structure and rework the stopwords macro
+      property data : AllLanguagesModel
+      # property stop_words
+      DEFAULT_MODELS_PATH = "#{__DIR__}/data/all.json"
+
+      def initialize(models_path = DEFAULT_MODELS_PATH)
+        json = File.read(DEFAULT_MODELS_PATH)
+        @data = AllLanguagesModel.from_json(json)
+        # if custom_stop_words.nil?
+        # language_word_emotion_score_hash.keys.each
+        # end
+      end
+
+      def analyse(text : String, language = "en")
+        # TODO take care of languages !
+        bag_of_words = Cadmium::Tokenizer::Aggressive.new.tokenize(text)
+        raw_result = Hash(Emotion, Float32).new
+        Emotion.values.each do |emotion|
+          raw_result[emotion] = 0.to_f32
+        end
+        normalizer = 0
+        bag_of_words.each do |word|
+          if emotion = @data.language_word_emotion_score_hash[language].has_key?(word)
+            normalizer += 1
+            emotion = @data.language_word_emotion_score_hash[language][word][0]
+            score = @data.language_word_emotion_score_hash[language][word][1]
+            raw_result[emotion] += score
+          end
+        end
+        raw_result.transform_values! do |score|
+          score / normalizer
+        end
+        raw_result.to_a.sort_by { |key, _| key }.to_h
+      end
+    end
+  end
+end
