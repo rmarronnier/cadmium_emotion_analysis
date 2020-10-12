4b5853bed92ea0ce87818709c1f3dc298464c5e4
Rémy Marronnier <rmarronnier@gmail.com>
2020-10-12 19:43:03 +0200

    fix

--- src/cadmium/emotion_analysis/emotion_analysis.cr
+++ src/cadmium/emotion_analysis/emotion_analysis.cr
@@ -49,7 +49,7 @@ module Cadmium
         # end
       end
 
-      def analyse(text : String, language = "en") : Hash(Emotion, Float32)
+      def analyse(text : String, language = "en") : Hash(String, Float32)
         # TODO take care of languages !
         bag_of_words = Cadmium::Tokenizer::Aggressive.new.tokenize(text)
         raw_result = Hash(Emotion, Float32).new
@@ -68,7 +68,7 @@ module Cadmium
         raw_result.transform_values! do |score|
           score / normalizer
         end
-        reuslt = raw_result.to_a.sort_by { |key, _| key }.to_h.transform_keys { |emotion| emotion.to_s }
+        result = raw_result.to_a.sort_by { |key, _| key }.to_h.transform_keys { |emotion| emotion.to_s }
         result
       end
     end
